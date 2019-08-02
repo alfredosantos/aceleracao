eeef10f1ac7b41804516949f2e6a4d6ccd253a44
alfredgs <alfredgsantos@gmail.com>
2019-08-02 13:25:31 -0300

    Updating Fibo is

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -5,26 +5,22 @@ import java.util.List;
 public class Fibonacci {
 
   static List fibonacci() {
-    int n = 0;
-    final List<Integer> list = new ArrayList<>();
-    Collections.addAll(list, 0, 1);
-    while(true){
-      final int last = list.get(list.size() - 1);
-      final int beforeLast = list.get(list.size() - 2);
-      final int next = last + beforeLast;
-      if (next > 350) {
-        break;
-      }
-      list.add(next);
-    }
-    return list;
+	int n = 0;
+	List<Integer> list = new ArrayList<>();
+	Collections.addAll(list, 0, 1);
+	while(true){
+	  final int last = list.get(list.size() - 1);
+	  final int beforeLast = list.get(list.size() - 2);
+	  final int next = last + beforeLast;
+	  if (next > 350) {
+		break;
+	  }
+	  list.add(next);
+	}
+	return list;
   }
 
   static Boolean isFibonacci(Integer n) {
-    if (n.equals(0) | n.equals(null)) {
-      throw new IllegalArgumentException("Entrada sem valor para descriptografar.");
-    }
-    final List<Integer> list = fibonacci();
-    return list.contains(n);
-  }
+	final List<Integer> list = fibonacci();
+	return list.contains(a);
 }
